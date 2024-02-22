ba7fc17ce0523f487812c61725c8ba1acacb938c
dxlerYT <73416867+dxlerYT@users.noreply.github.com>
2024-02-22 23:50:58 +0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+//import React from "react"; not needed
+import './App.css'
+import Student from "./Student"
+const App = ()=>{
+  return(
+    <div className="App">
+      <h1>Azmi says hello to react</h1>
+      <Student />
+      <footer className='footer'>
+        Made by Azmi
+      </footer>
+
+    </div>
+  )
+}
+export default App
---
+++ src/Student.jsx
@@ -0,0 +1,15 @@
+//rafce auto type
+const Student = () => {
+  return (
+    <div>
+      <h3>Student Component</h3>
+      <ul>
+        <li>Student Name: Azmi</li>
+        <li>StudentID: 123</li>
+        <li>Azmi is not a graduate student</li>
+      </ul>
+      <img src="src/download.png" alt="ReactJS Icon"></img>
+    </div>
+  );
+}
+export default Student;
